b5e76c090de2293985ac88e53d04061d3debf71c
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-05-13 17:22:40 +0200

    Created InvalidPacketException.

---
+++ client/crystalgo/client/InvalidPacketException.java
@@ -0,0 +1,23 @@
+package crystalgo.client;
+
+/**
+ * Represents an invalid packet sent by the server.
+ * Created by user on 13/05/16.
+ */
+public class InvalidPacketException extends Exception {
+    public InvalidPacketException() {
+        super();
+    }
+
+    public InvalidPacketException(String s) {
+        super(s);
+    }
+
+    public InvalidPacketException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public InvalidPacketException(Throwable throwable) {
+        super(throwable);
+    }
+}
