1a0ee5b143597b5cb3bb74921e3c6c42d8b37409
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-05-15 14:58:28 +0200

    Changed over to winner in GoState.

--- client/crystalgo/client/engine/GoState.java
+++ client/crystalgo/client/engine/GoState.java
@@ -17,19 +17,19 @@ public final class GoState {
     private final GoState parent;
     private final int depth;
     private final Move move;
-    private final boolean over;
+    private final Role winner;
 
-    public GoState(Role turn, Board board, GoState parent, int depth, Move move, boolean over) {
+    public GoState(Role turn, Board board, GoState parent, int depth, Move move, Role winner) {
         this.turn = turn;
         this.board = board;
         this.parent = parent;
         this.depth = depth;
         this.move = move;
-        this.over = over;
+        this.winner = winner;
     }
 
     public static GoState newGame(Board board) {
-        return new GoState(Role.black, board, null, 0, null, false);
+        return new GoState(Role.black, board, null, 0, null, Role.spectate);
     }
 
     public Role getTurn() {
@@ -52,10 +52,6 @@ public final class GoState {
         return Optional.ofNullable(move);
     }
 
-    public GoState gameIsOver() {
-        return new GoState(turn, board, parent, depth, move, true);
-    }
-
     /**
      * Performs the move in the argument. This function does not check if a move is invalid.
      * @param move The move to perform.
@@ -63,12 +59,23 @@ public final class GoState {
      */
     public GoState doMove(Move move) {
         Board b = board.setSpot(move.x, move.y, getTurn().spotcolor);
-        return new GoState(turn.inverse(), b, this, depth + 1, move, over);
+        return new GoState(turn.inverse(), b, this, depth + 1, move, winner);
     }
 
-    public boolean isGameOver() {
-        return over;
+    /**
+     * Returns the role which won this go game.
+     * Returns spectate if the game is not yet over.
+     *
+     * @return The role which won the game, or spectate if the game is't over.
+     */
+    public Role getWinner() {
+        return winner;
     }
+
+    public GoState setWinner(Role winner) {
+        return new GoState(turn, board, parent, depth, move, winner);
+    }
+
 
 }
 
